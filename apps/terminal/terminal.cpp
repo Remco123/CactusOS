d74100cb9e70be030e9911223ec009e966578ab1
Remco van Homelen <remcovanhomelen@gmail.com>
2019-07-19 12:19:34 +0200

    Removed test code

--- apps/terminal/terminal.cpp
+++ apps/terminal/terminal.cpp
@@ -10,8 +10,6 @@ int main()
     GUI::Initialize();
 
     Window* mainWindow = new Window(300, 200, WIDTH/2 - 150, HEIGHT/2 - 100);
-    Label* demoText = new Label("dawwwwwwwoaiwdisjadkawdjiajsk\ndawoidoapsidpoawidoiaosido\nwoadiosakmkcmapwi990\nad9aw8u8u127shduwhuihd\n&YIUHUYOIJHJHUYUTYGJBFRQPAMCUWGUI");
-    mainWindow->childs.push_back(demoText);
     
     GUI::MakeAsync();
     while(1)
